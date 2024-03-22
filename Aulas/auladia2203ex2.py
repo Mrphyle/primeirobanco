c7b2b159373fb1ea2265f7a0ea5a00f25bc0ae0b
Mrphyle~ <diegosimogoncalves18@gmai.com>
2024-03-22 18:00:52 -0300

    sim

--- Aulas/auladia2203ex2.py
+++
@@ -1,6 +0,0 @@
-ensemble = {'maça','pera','uva','caqui','jaca','ameixa','jambo'}
-for fruit in ensemble:
-    print(fruit)
-    ensemble.add(input("Digite uma fruta"))
-    for fruit in ensemble:
-        print(fruit)
