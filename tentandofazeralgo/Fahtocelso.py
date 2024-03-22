d902a43235d64ca5465330d01981f398e38e278d
Mrphyle <diegosimaogoncalves18@gmail.com>
2024-03-22 20:10:02 -0300

    fah para celso portioli

---
+++ tentandofazeralgo/Fahtocelso.py
@@ -0,0 +1,4 @@
+Fah = float(input("type number in Fah "))
+resultinC = (5/9) * (Fah - 32)
+RoundresultinC = round(resultinC)
+print(RoundresultinC)
--- testedepuchar.py
+++
@@ -1,4 +0,0 @@
-Fah = float(input("type number in Fah "))
-resultinC = (5/9) * (Fah - 32)
-RoundresultinC = round(resultinC)
-print(RoundresultinC)
