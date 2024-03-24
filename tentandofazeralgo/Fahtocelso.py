b41dde7f36f8caa9be0f1924409e8498f5c2c872
Mrphyle <diegosimaogoncalves18@gmail.com>
2024-03-24 10:41:09 -0300

    restart mode on

--- tentandofazeralgo/Fahtocelso.py
+++ tentandofazeralgo/Fahtocelso.py
@@ -1,4 +1,10 @@
-Fah = float(input("type number in Fah "))
-resultinC = (5/9) * (Fah - 32)
-RoundresultinC = round(resultinC)
-print(RoundresultinC)
+def ConvertTemp():
+    Fah = float(input("type number in Fah "))
+    resultinC = (5/9) * (Fah - 32)
+    RoundresultinC = round(resultinC)
+    print(RoundresultinC)
+while True:
+    ConvertTemp()
+    restartcont = input("Do another calculation? ")
+    if restartcont.lower() != 'Yes' and restartcont.lower() != 'yes':
+        break
