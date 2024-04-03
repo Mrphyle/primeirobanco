ab3248a8c65afea3b71cd00ed3c7696142ee7905
Mrphyle <163591079+Mrphyle@users.noreply.github.com>
2024-04-03 14:13:20 -0300

    Update codigos test3.py

--- Codigos test/codigos test3.py
+++ Codigos test/codigos test3.py
@@ -1,10 +1,16 @@
 UsageElevator = input("qual elevador você usa o A,B ou C? ")
 periodo = input("(M matutino/ V vespertino e N noturno)\nQual horario você usa? ")
-if UsageElevator.lower() == 'a':
-    saveA = UsageElevator
-elif UsageElevator.lower() == 'b':
-    saveB= UsageElevator
-else:
-    saveC= UsageElevator
+def Elevator():
+    if UsageElevator.lower() == 'a':
+        saveA = UsageElevator
+    elif UsageElevator.lower() == 'b':
+        saveB= UsageElevator
+    else:
+        saveC= UsageElevator
+def Periodo():
+    if periodo.lower == 'm'
+        saveM = periodo
+
+
 
 
