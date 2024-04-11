61113628354a581f71a622329a34f92967a2de0c
Mrphyle <diegosimaogoncalves18@gmail.com>
2024-04-10 21:01:04 -0300

    10/04/24

--- Codigos test/codigos test4.py
+++ Codigos test/codigos test4.py
@@ -1,27 +1,19 @@
-import numpy as np
-import matplotlib.pyplot as plt
-categorias = ['1', '2', '3', '4', '5','6']
-valores_yes = [6, 7, 5, 6, 4, 5]
-valores_no = [4, 3, 5, 4, 6, 5]
-x = np.arange(len(categorias))
-largura_barra = 0.35
-fig, ax = plt.subplots()
-barras_yes = ax.bar(x - largura_barra/2, valores_yes, largura_barra, label='Yes')
-barras_no = ax.bar(x + largura_barra/2, valores_no, largura_barra, label='No')
-ax.set_title('What healthy habits do you practice?')
-ax.set_xlabel('Questions')
-ax.set_ylabel('Quantity')
-ax.set_xticks(x)
-ax.set_xticklabels(categorias)
-ax.legend()
-def autolabel(barras):
-    for barra in barras:
-        altura = barra.get_height()
-        ax.annotate('{}'.format(altura),
-                    xy=(barra.get_x() + barra.get_width() / 2, altura),
-                    xytext=(0, 3),
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-autolabel(barras_yes)
-autolabel(barras_no)
-plt.show()
+Login = input("Faça assim:(Exemplo:Diego dia 10/04/24)\n\nQual o nome e dia de emição do pedido? ")
+Quantia = float(input("Qual a quantidade de placas que desejá? "))
+Largurasign = float(input("Qual a largura da placa? "))
+Alturasign = float(input("Qual a altura da placa? "))
+Script = input("O que será escrito? ")
+removespace = (Script.strip().replace(" "," "))
+Corsign = input("Qual cor você desejá branco ou cinza? ")
+print("___________________________")
+CorScript = input("azul\nvermelho\namarelo\npreto\nverde\nQual das cores acima será escrito a frase? ")
+def ValorTotal():
+    Areasign = (Largurasign*Alturasign)
+    CustoMaterial = Areasign * 147.30
+    CustoDesenho = (len(removespace)* 0.37)
+    ValorPlaca =(CustoMaterial+CustoDesenho)
+    return ValorPlaca
+Prazo = (Quantia/6)
+total = ValorTotal() * Quantia
+print(f"_______________________________\nCadastro: {Login}\nQuantidade: {Quantia}\ntamanho: {Alturasign} x {Largurasign}\nFrase: {Script}\nCor da placa: {Corsign}\nCor da letra: {CorScript}")
+print(f"_______________________________\nArea:{Largurasign*Alturasign}\nMaterial:{(Largurasign*Alturasign) * 147.30}\nfrase:{len(removespace)* 0.37}\nValor total:{total}\n_______________________________")
--- PASS-codigo-test/YoutubeDownloader.py
+++ PASS-codigo-test/YoutubeDownloader.py
@@ -1,8 +1,16 @@
 from pytube import YouTube
-url = input("Coloque a URL do vídeo: ")
-video = YouTube(url)
-print('Baixando video....')
-download = video.streams.get_highest_resolution()
-downloadpath = r"C:\Users\hoolf\Videos"
-download.download(downloadpath)
-print('O download foi um sucesso!')
+def youtubedowloader():
+    url = input("Coloque a URL do vídeo: ")
+    video = YouTube(url)
+    print('Baixando video....')
+    download = video.streams.get_highest_resolution()
+    downloadpath = r"C:\Users\hoolf\Videos"
+    download.download(downloadpath)
+    print('O download foi um sucesso!')
+while True:
+    youtubedowloader()
+    ResetTube = input("Deseja baixar outro video? ")
+    print("___________________________________")
+    if ResetTube.lower() not in ['yes', 'sim', '1']:
+        print('Encerrado')
+        break
