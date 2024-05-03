d3fa7005e0885f21fc2d543e5217559ddf339a46
Mrphyle <163591079+Mrphyle@users.noreply.github.com>
2024-05-03 10:05:51 -0300

    Update OpenApps.py

--- OpenApps.py
+++ OpenApps.py
@@ -1,113 +1,50 @@
 import os
-def codereset():
-    ops = input("subjuntos\nAub\nAnb\ncomplemento\nQual deseja das opições? ")
-    if ops.lower() == 'subconjunto':
-        def subconjunto():
-            ops1 = int(input("Desejá quantos elementos? "))
-            A = set()
-            for i in range(ops1):
-                save = int(input("Digite o elemento: "))
-                A.add(save)
-            Subconjunto = len(A)
-            print(f"{A}\nQuantindade de subconjunto é igual 2**{Subconjunto} ={2**Subconjunto}")
-        while True:
-            subconjunto()
-            restcont = input("Deseja realizar a conta novamente? ")
-            if not restcont.lower() in ['sim','yes','1']:
-                break
-    elif ops.lower() == 'aub':
-        def AUB():    
-            typeaub = input("AuB ou AuBuC\nDesejá fazer qual das opiçoes a cima? ")
-            if typeaub.lower() == 'aub':
-                ops1 = int(input("Desejá quantos elementos? "))
-                A = set()
-                for i in range(ops1):
-                    save1 = int(input("Digite o elemento: "))
-                    A.add(save1)
-                ops2 = int(input("Desejá quantos elementos? "))
-                B = set()
-                for i in range(ops2):
-                    save2 = int(input("Digite o elemento: "))
-                    B.add(save2)
-                print(f"A= {A}\nB={B}\nAUB={A.union(B)}")
-            elif typeaub.lower() == 'aubuc':
-                ops1 = int(input("Desejá quantos elementos? "))
-                A = set()
-                for i in range(ops1):
-                    save1 = int(input("Digite o elemento: "))
-                    A.add(save1)
-                ops2 = int(input("Desejá quantos elementos? "))
-                B = set()
-                for i in range(ops2):
-                    save2 = int(input("Digite o elemento: "))
-                    B.add(save2)
-                C = set()
-                ops3 = int(input("Desejá quantos elementos? "))
-                for i in range(ops3):
-                    save3 = int(input("Digite o elemento: "))
-                    C.add(save3)
-                print(f"A= {A}\nB={B}\nC={C}\nAUB={A.union(B and C)}")
-        while True:
-            AUB()
-            restcont = input("Deseja realizar a conta novamente? ")
-            if not restcont.lower() in ['sim','yes','1']:
-                break
-    elif ops.lower() == 'anb':
-        def ANB():
-            typeanb = input("AnB ou AnBnC\nDesejá fazer qual das opiçoes a cima? ")
-            if typeanb.lower() == 'anb':
-                ops1 = int(input("Desejá quantos elementos? "))
-                A = set()
-                for i in range(ops1):
-                    save1 = int(input("Digite o elemento: "))
-                    A.add(save1)
-                ops2 = int(input("Desejá quantos elementos? "))
-                B = set()
-                for i in range(ops2):
-                    save2 = int(input("Digite o elemento: "))
-                    B.add(save2)
-                print(f"A= {A}\nB={B}\nAUB={A.intersection(B)}")
-            elif typeanb.lower() == 'anbnc':
-                ops1 = int(input("Desejá quantos elementos? "))
-                A = set()
-                for i in range(ops1):
-                    save1 = int(input("Digite o elemento: "))
-                    A.add(save1)
-                ops2 = int(input("Desejá quantos elementos? "))
-                B = set()
-                for i in range(ops2):
-                    save2 = int(input("Digite o elemento: "))
-                    B.add(save2)
-                ops3 = int(input("Desejá quantos elementos? "))
-                C = set()
-                for i in range(ops3):
-                    save3 = int(input("Digite o elemento: "))
-                    C.add(save3)
-                print(f"A= {A}\nB={B}\nC={C}\nAUB={A.intersection(B and C)}")
-        while True:
-            ANB()
-            restcont = input("Deseja realizar a conta novamente? ")
-            if not restcont.lower() in ['sim','yes','1']:
-                break
-    elif ops.lower() == 'complemento':
-        def comp():
-            ops1 = int(input("Desejá quantos elementos? "))
-            A = set()
-            for i in range(ops1):
-                save1 = int(input("Digite o elemento: "))
-                A.add(save1)
-            ops2 = int(input("Desejá quantos elementos? "))
-            B = set()
-            for i in range(ops2):
-                save2 = int(input("Digite o elemento: "))
-                B.add(save2)
-            print(f"A= {A}\nB={B}\nAUB={(A - B)}")
+import webbrowser
+import pyperclip
+def whatappopen():
+    pyperclip.copy("https://chat.openai.com/")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    WhatAppOpen = input("_________________________________\nNavegadores(Exibe lista de navegadors)\nNavegador P(Padrão)\nSpotify\nDeepl\nAbrir um site\nSpeedtest\nQual destes acima deseja iniciar? ")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    def navegs():
+        if WhatAppOpen.lower() == 'navegadores':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            BrownserOps = input("Google\nOperaGX\nEdge\nQual dos navegadores disponiveis acima deseja? ")
+            if BrownserOps.lower() == 'google' :
+                os.startfile(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
+                print("\nAbrindo Google chorome....")
+            elif BrownserOps.lower() in ['opera gx','operagx']:
+                os.startfile(r"C:\Users\hoolf\AppData\Local\Programs\Opera GX\launcher.exe")
+                print("\nAbrindo Opera Gx....")
+            else:
+                pyperclip.copy("goncalves1807")
+                webbrowser.get(r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe").open("https://teams.microsoft.com/_?culture=pt-br&country=br#/school/teams-grid/General?ctx=teamsGrid")
+                print('\nAbrindo Microsoft Edge.....')
+    while True:
+        navegs()
+        restartcont = input("\nDeseja abrir outro navegador? ")
+        if not restartcont.lower() in ['yes','sim','1']:
+            print("________________________\n")
+            break
+    if WhatAppOpen.lower() == 'Navegador P(Padrão)':
+        os.startfile(r"C:\Users\hoolf\AppData\Local\Programs\Opera GX\launcher.exe")
+        print("\nAbrindo Opera Gx....")
+    if WhatAppOpen.lower() == 'spotify':
+        os.startfile(r"C:\Users\hoolf\AppData\Roaming\Spotify\Spotify.exe")
+        print("\nAbrindo spotify....")
+    if WhatAppOpen.lower() == 'deepl':
+        os.startfile(r"C:\Users\hoolf\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\DeepL")
+        print("\nAbrindo Deepl....")
+    if WhatAppOpen.lower() == 'speedtest':
+        os.startfile(r"C:\Program Files\Speedtest\Speedtest.exe")
+        print("\nAbrindo speedtest")
+    if WhatAppOpen.lower() == 'site':
+        web = input("Digite a url")
+        webbrowser.open(web)
+        print(f"\nAbrindo o site:{web}")
 while True:
-    codereset()
-    resetcode = input("Desejá fazer outra operação? ")
-    Del = input("Desejá limpar as contas anteriores? ")
-    if Del.lower() in ['sim','yes','1']:
-        os.system('cls' if os.name == 'nt' else 'clear')
-    if not resetcode.lower in ['sim','yes','1']:
-        print("________\nCodigo finalizado!")
+    whatappopen()
+    restartcont = input("\nDeseja abrir outro aplicativo? ")
+    if not restartcont.lower() in ['yes','sim','1']:
+        print('___________________\nComando finalizado')
         break
