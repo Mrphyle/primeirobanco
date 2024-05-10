80ce0083d54d46ae541614c5f94894854bb28af0
Mrphyle <diegosimaogoncalves18@gmail.com>
2024-05-10 11:40:30 -0300

    update

---
+++ Python power up/aula4.py
@@ -0,0 +1,27 @@
+import flet as ft
+#Titulo Registro de produto
+def main(pag):
+    #Titulo Registro de produto
+    Title =ft.Text("Rigistro de produtos da Bookencadernadora")
+    pag.add(Title)
+    #Nome do produto
+    ProdutoNome = ft.TextField(label="Nome do produto")
+    ProdutoNome.value
+    pag.add(ProdutoNome)
+    #adicionar nova categoria
+    Addcat = ft.TextField(label="Quer adicionar uma nova categoria?" )
+    catlist = ['Album','Pendrive']
+    catlist.append(Addcat.value)
+    #categoria
+    Categoria= ft.TextField(label="Categoria" )
+    catlist = ['Album','Pendrive']
+    #adicionar nova categoria
+    catlist.append(Addcat.value)
+    catlistp = ft.Text(catlist)
+    pag.update()
+    pag.add(Addcat)
+    pag.add(catlistp,Categoria)
+    #valor
+    #enviar tudo para um banco de dados
+    enviar = ft.ElevatedButton("Enviar")
+ft.app(main)#,view=ft.WEB_BROWSER)
