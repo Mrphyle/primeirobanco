a6c8354cb1e98e634062f30d0de75040efe0393c
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-05-24 07:56:07 -0300

    var1.0

---
+++ Avaliação/OBI treinamento/var.py
@@ -0,0 +1,8 @@
+EntradaDoAppX = input("Digite o primeiro numero: ")
+EntradaDoAppY = input("Digite o segundo numero: ")
+campoX = 8
+campoY = 16
+if campoX<=8 and campoX>=0 and campoY<=16 and campoY>=0:
+    print("S")
+else:
+    print("N")
