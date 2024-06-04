d392c4ff9f937f36335e69a32e626a04d7f5f397
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-06-04 11:52:39 -0300

    openapp2 ver1.2 add wappaper button,clipstudo,huionhs64 and steam

--- OpenApp2.py
+++ OpenApp2.py
@@ -15,8 +15,20 @@ class Buttontype:
         os.startfile(obsfile)
     @staticmethod
     def Wallpaperengine_location():
-        wallpaperenfile = r""
+        wallpaperenfile = r"C:\Users\hoolf\Desktop\Wallpaper Engine.url"
         os.startfile(wallpaperenfile)
+    @staticmethod
+    def Clippaint_location():
+        clipstudiofile = r"C:\Program Files\CELSYS\CLIP STUDIO 1.5\CLIP STUDIO PAINT\CLIPStudioPaint.exe"
+        os.startfile(clipstudiofile)
+    @staticmethod
+    def Huionh64_locate():
+        huionhs64file = r"C:\Program Files\HuionTablet\HuionTablet.exe"
+        os.startfile(huionhs64file)
+    @staticmethod
+    def steam_locate():
+        steamfile= r"C:\Program Files (x86)\Steam\steam.exe"
+        os.startfile(steamfile)
 # Criar a janela principal
 root = tk.Tk()
 root.geometry("800x600")
@@ -42,6 +54,18 @@ spotify_button.place(x=20,y=1*75)
 chrome_button = tk.Button(root, text="Google Chrome", command=Buttontype.chrome_location, **button_style)
 chrome_button.place(x=20,y=2*75)
 #Obs_location
-obsbutton = tk.Button(root,text="OBS",command=Buttontype.Obs_location,**button_style)
-obsbutton.place(x=20,y=3*75)
+obs_button = tk.Button(root,text="OBS",command=Buttontype.Obs_location,**button_style)
+obs_button.place(x=20,y=3*75)
+#wallpaperengine
+wallpprengbutton = tk.Button(root,text="wallpaper\nengine",command=Buttontype.Wallpaperengine_location,**button_style)
+wallpprengbutton.place(x=225,y=3*75)
+#clipstudio
+clipstudiopaint_button = tk.Button(root, text="ClipStudio\nPaint",command=Buttontype.Clippaint_location, **button_style)
+clipstudiopaint_button.place(x=225,y=75)
+#huionsoftuware
+huionhs64_button = tk.Button(root,text="Huion hs64\nSoftuware",command=Buttontype.Huionh64_locate,**button_style)
+huionhs64_button.place(x=225,y=2*75)
+#steam
+steam_button = tk.Button(root,text="Steam",command=Buttontype.steam_locate,**button_style)
+steam_button.place(x=225,y=4*75)
 root.mainloop()
