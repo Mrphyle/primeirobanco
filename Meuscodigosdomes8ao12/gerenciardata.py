3821cdeea5a73caf9dea728a034250f3baa6b532
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-08-16 13:05:11 -0300

    Update gerenciardata.py

--- Meuscodigosdomes8ao12/gerenciardata.py
+++ Meuscodigosdomes8ao12/gerenciardata.py
@@ -22,7 +22,32 @@ except:
 finally:
     cursor.close()
     connect.close()
-cor = ['red','yellow','green']
+cor = ['white','yellow']
 plt.bar(x,y,width=0.5,color=cor)
 plt.title("quantidade de naves")
 plt.show()
+'''
+import mysql.connector as mysql
+import matplotlib.pyplot as plt
+x = []
+y= []
+try:
+    connect = mysql.connect(
+        host = 'localhost',
+        user= 'root',
+        password = '',
+        database = 'mrphyle',
+    )
+    sql = "SELECT marca, COUNT(marca) FROM veiculos GROUP BY marca;"
+    cursor = connect.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for line in result:
+        y.append(line[1])
+        x.append(line[0])
+except:
+    print('ERROR!!')
+cor = ['red','yellow','green']
+plt.bar(x,y,width=0.5,color=cor)
+plt.title("quantidade de naves")
+plt.show()'''
